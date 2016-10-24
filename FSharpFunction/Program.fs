dbdedebe117e4a80f164292a4d675e548c79e1f6
Robin <robinpauquet@gmail.com>
2016-10-24 15:52:16 +0200

    first commit v1.0

---
+++ FSharpFunction/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let bonjour = 
+    printfn "Coucou"
+
+let bonjourToi prenom = 
+    printfn "Coucou %s" prenom
+
+let bonjourToiString prenom : string = 
+    sprintf "Coucou %s" prenom
+
+let bonjourVous prenom prenom2 = 
+    printfn "Coucou %s et %s" prenom prenom2
+
+[<EntryPoint>]
+let main argv = 
+    
+    bonjour
+    bonjourToi "toto"
+    printfn "%s" (bonjourToiString "toto2")
+    bonjourVous "toto" "tata"
+
+    let stop = Console.ReadLine()
+
+    1
